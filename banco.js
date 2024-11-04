a79442c261e08d83a3ad82dbc7dde9af22544d54
Rolando_cf <125164239+Rolando-Cepeda@users.noreply.github.com>
2024-11-04 19:39:54 +0100

    Creación de formulario

--- banco.js
+++ banco.js
@@ -11,7 +11,7 @@ function form() {
 
 	let labelName = document.createElement("label"); // Se crea el LABEL
 	labelName.setAttribute("for", "name")//Aqui se ponen los atributos del label.
-	labelName.textContent = "Nombre: " 
+	labelName.textContent = "Nombre: " // Añadimos el nombre de la etiqueta label
 	let inputName = document.createElement("input"); //  Creamos un input
 	inputName.setAttribute("type", "text"); //Ponemos los atributos que llevaran los INPUTS.
 	inputName.setAttribute("id", "name");
@@ -19,6 +19,7 @@ function form() {
 
 	let labelApellido = document.createElement("label");
 	labelApellido.setAttribute("for", "apellido")
+	labelApellido.textContent = "Apellido: "
 	let inputApellido = document.createElement("input");
 	inputApellido.setAttribute("type", "text");
 	inputApellido.setAttribute("id", "apellido");
@@ -26,6 +27,7 @@ function form() {
 
 	let labelDireccion = document.createElement("label");
 	labelDireccion.setAttribute("for", "direccion");
+	labelDireccion.textContent = "Direccion: ";
 	let inputDireccion = document.createElement("input");
 	inputDireccion.setAttribute("type", "text");
 	inputDireccion.setAttribute("id", "direccion");
@@ -34,20 +36,25 @@ function form() {
 
 	let labelEmail = document.createElement("label");
 	labelEmail.setAttribute("for", "email");
+	labelEmail.textContent = "Email: ";
 	let inputEmail = document.createElement("input");
 	inputEmail.setAttribute("type", "email");
 	inputEmail.setAttribute("id", "email");
 	inputEmail.setAttribute("required", true);
+	inputEmail.setAttribute("pattern",  "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$");
 
 	let labelConfirmEmail = document.createElement("label");
 	labelConfirmEmail.setAttribute("for", "confirmEmail");
+	labelConfirmEmail.textContent = "Confirmar Email: ";
 	let inputConfirmEmail = document.createElement("input");
 	inputConfirmEmail.setAttribute("type", "email");
 	inputConfirmEmail.setAttribute("id", "confirmEmail");
 	inputConfirmEmail.setAttribute("required", true);
+	inputConfirmEmail.setAttribute("pattern",  "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$");
 
 	let labelDni =  document.createElement("label");
 	labelDni.setAttribute("for", "dni");
+	labelDni.textContent = "DNI: ";
 	let inputDni = document.createElement("input");
 	inputDni.setAttribute("type", "text");
 	inputDni.setAttribute("id", "dni");
@@ -55,6 +62,7 @@ function form() {
 
 	let  labelIntereses = document.createElement("label");
 	labelIntereses.setAttribute("for", "intereses");
+	labelIntereses.textContent = "Intereses: ";
 	let inputIntereses = document.createElement("input");
 	inputIntereses.setAttribute("type", "text");
 	inputIntereses.setAttribute("id", "intereses");
@@ -62,6 +70,7 @@ function form() {
 
 	let labelPassWord = document.createElement("label");
 	labelPassWord.setAttribute("for", "password");
+	labelPassWord.textContent = "Contraseña: ";
 	let inputPassWord = document.createElement("input");
 	inputPassWord.setAttribute("type", "text");
 	inputPassWord.setAttribute("id", "password");
@@ -71,22 +80,35 @@ function form() {
 
 	crearFormulario.appendChild(labelName); //  Agregamos el label al formulario
 	crearFormulario.appendChild(inputName); //  Agregamos el input al formulario
-	lineBreak(crearFormulario, 1);
+	lineBreak(crearFormulario, 2);
 
 	crearFormulario.appendChild(labelApellido);
 	crearFormulario.appendChild(inputApellido);
+	lineBreak(crearFormulario, 2);
+
 	crearFormulario.appendChild(labelDireccion);
 	crearFormulario.appendChild(inputDireccion);
+	lineBreak(crearFormulario, 2)
+
 	crearFormulario.appendChild(labelEmail); 
 	crearFormulario.appendChild(inputEmail);
+	lineBreak(crearFormulario, 2);
+
 	crearFormulario.appendChild(labelConfirmEmail); //  Agregamos el label al formulario
 	crearFormulario.appendChild(inputConfirmEmail); //  Agregamos el input al formulario
+	lineBreak(crearFormulario, 2);
+
 	crearFormulario.appendChild(labelDni); //  Agregamos el label al formulario
 	crearFormulario.appendChild(inputDni); //  Agregamos el input al formulario
+	lineBreak(crearFormulario, 2);
+
 	crearFormulario.appendChild(labelIntereses);
 	crearFormulario.appendChild(inputIntereses);
+	lineBreak(crearFormulario, 2);
+
 	crearFormulario.appendChild(labelPassWord); 
 	crearFormulario.appendChild(inputPassWord);
+	lineBreak(crearFormulario, 2);
 
 
 
@@ -95,3 +117,7 @@ function form() {
 }
 
 form();
+
+function limitations() {
+	ecent
+}
